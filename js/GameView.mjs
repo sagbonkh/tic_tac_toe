55bc5c9eb0d0cb721261e77906243f896ec55fca
sagbonkh <sidneyagbonkhese9@gmail.com>
2022-01-17 21:15:49 -0800

    Completed board design

--- js/GameView.mjs
+++ js/GameView.mjs
@@ -13,6 +13,18 @@ export default class GameView{
                     <i class="material-icons"> refresh </i>
                 </button>
             </div>
+            <div class= "board">
+                <div class= "board__tile" data-index= "O">O</div>
+                <div class= "board__tile" data-index= "1">O</div>
+                <div class= "board__tile board__tile--winner" data-index= "2">O</div>
+                <div class= "board__tile" data-index= "3">O</div>
+                <div class= "board__tile" data-index= "4">O</div>
+                <div class= "board__tile" data-index= "5">O</div>
+                <div class= "board__tile" data-index= "6">O</div>
+                <div class= "board__tile" data-index= "7">O</div>
+                <div class= "board__tile" data-index= "8">O</div>
         `;
+        this.onTileClick = undefined;
+        this.onRestartClick = undefined;
     }
 }
